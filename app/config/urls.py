5ab9c870cec5e602a6365de3b297c12347b606ce
marikkaratist <nebose@mail.ru>
2024-08-30 20:30:41 +0700

    M-8: Добавление ссылок для Swagger

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('realty/', include('realty.urls')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'))
 ]
 
 if settings.DEBUG:
